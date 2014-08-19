804af49d5d6d3b1c7a5ebda45b5e939a40012d0f
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-19 14:12:46 -0500

    finished the functions exercise

--- todo.php
+++ todo.php
@@ -1,54 +1,66 @@
 <?php
 
-// create array to hold list of todo items
-$items = array();
-// The loop!
-	echo "Welcome to my TODO list!" . PHP_EOL;
-do {
-	// iterate each item
-	foreach ($items as $key => $item) {
-		// display each item and a newline
-		echo "\t" . ($key + 1) . ". $item" . PHP_EOL;
-	}
-		echo "---------------------" . PHP_EOL;
+ // Create array to hold list of todo items
+ $items = array();
 
-	// show the menu options
-	echo "Magic Words: (N)ew item, (R)emove item, (Q)uit : ";
+ // List array items formatted for CLI
+ function list_items($list)
+ {
+    $list_string = '';
+    
+    foreach ($list as $key => $value) {
+        $list_string .= "\t" . ($key + 1) . ". $value" . PHP_EOL;
+    }
 
-	// get the unput from user
-	// use trim() to remove whitespaces and add newlines
-	$input = ucfirst(trim(fgets(STDIN)));
+     // Return string of list items separated by newlines.
+     // Should be listed [KEY] Value like this:
+     // [1] TODO item 1
+     // [2] TODO item 2 - blah
+     // DO NOT USE ECHO, USE RETURN
+     return $list_string;
+ }
 
-	// check for actionable input
-	if ($input == 'N') {
-		// ask for entry
-		echo 'Enter item: ';
-		// add entry to list array
-		$items[] = trim(fgets(STDIN));
-	} elseif ($input == "R") {
-		// Remove which item?
-		echo 'Enter item number to remove: ' . PHP_EOL;
-		 // Get array key
-        $key = trim(fgets(STDIN));
-        $key--;
-		// remove from array
-		unset($items[$key]);
-		// reset array index
-		$items = array_values($items);
-		// yells at user for trying to beat the system
-	} elseif ($input != "Q") {
-		echo "PERMISSION DENIED....and...." . PHP_EOL;
-		usleep(1000000);
-		// easter egg
-		for ($i=0; $i > -42; $i++) { 
-			echo "YOU DIDNT SAY THE MAGIC WORD!" . PHP_EOL;
-		}
-	}
-// exit when input is (Q)uit
-} while ($input !='Q');
+ // Get STDIN, strip whitespace and newlines, 
+ // and convert to uppercase if $upper is true
+ function get_input($upper = FALSE) 
+ {
+         $input = trim(fgets(STDIN));
+     return $input;
 
-// Say goodbye!
-echo "Goodbye!" . PHP_EOL;
+     // Return filtered STDIN input
+ }
 
-// Exit with 0 errors
-exit(0);
+ // The loop!
+ do {
+     // Echo the list produced by the function
+     echo list_items($items);
+
+     // Show the menu options
+     echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+     // Get the input from user
+     // Use trim() to remove whitespace and newlines
+     $input = get_input(TRUE);
+
+     // Check for actionable input
+     if ($input == 'N') {
+         // Ask for entry
+         echo 'Enter item: ';
+         // Add entry to list array
+         $items[] = get_input();
+     } elseif ($input == 'R') {
+         // Remove which item?
+         echo 'Enter item number to remove: ';
+         // Get array key
+         $key = get_input();
+         // Remove from array
+         unset($items[$key]);
+     }
+ // Exit when input is (Q)uit
+ } while ($input != 'Q');
+
+ // Say Goodbye!
+ echo "Goodbye!\n";
+
+ // Exit with 0 errors
+ exit(0);
